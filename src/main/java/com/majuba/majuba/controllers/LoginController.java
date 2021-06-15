d4ec85676d9f271a7a58044be49c2b616232f244
bhuergo <barbarahuergo@gmail.com>
2021-06-15 16:46:31 -0300

    GetMapping en LoginController

--- src/main/java/com/majuba/majuba/controllers/LoginController.java
+++ src/main/java/com/majuba/majuba/controllers/LoginController.java
@@ -1,8 +1,16 @@
 package com.majuba.majuba.controllers;
 
+import com.majuba.majuba.entities.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class LoginController {
-
+    @GetMapping("/login")
+    public ModelAndView login() {
+        ModelAndView mav = new ModelAndView("login");
+        mav.addObject("user", new User());
+        return mav;
+    }
 }
