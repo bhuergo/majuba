2e5605a50c75460e70522e4204625d93b8b3c041
bhuergo <barbarahuergo@gmail.com>
2021-06-17 20:40:39 -0300

    Query nativa

--- src/main/java/com/majuba/majuba/repositories/TableRepository.java
+++ src/main/java/com/majuba/majuba/repositories/TableRepository.java
@@ -13,7 +13,7 @@ public interface TableRepository extends JpaRepository<Table, Long>{
     @Query("SELECT COUNT (t) FROM Table t WHERE t.capacity >= :num_guests AND t.available = true")
     Integer num_tables(@Param("num_guests") Integer num_guests);
 
-    @Query("SELECT t FROM Table t WHERE t.capacity >= :num_guests AND t.available = true HAVING MIN(t.capacity)")
+    @Query(value = "SELECT t.* FROM Table t WHERE t.capacity >= :num_guests AND t.available = true HAVING MIN(t.capacity)", nativeQuery = true)
     Table assigned_table(@Param("num_guests") Integer num_guests);
 
 }
