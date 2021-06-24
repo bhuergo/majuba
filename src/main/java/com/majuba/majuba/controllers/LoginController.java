ba369a950150ae2667a45aa904cdfc996587ee56
gallartj <juanpablogallart@gmail.com>
2021-06-24 12:46:05 -0300

    LOGUIN TERMINADO BITCHES

--- src/main/java/com/majuba/majuba/controllers/LoginController.java
+++ src/main/java/com/majuba/majuba/controllers/LoginController.java
@@ -5,6 +5,13 @@ import com.majuba.majuba.entities.User;
 import com.majuba.majuba.services.TableService;
 import com.majuba.majuba.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,12 +21,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.security.Principal;
 
 @Controller
 
 public class LoginController {
+
+    @Autowired
+    private AuthenticationManager authManager ;
 
     @Autowired
     private TableService tableService;
@@ -42,10 +53,8 @@ public class LoginController {
 
     //Al apretar "ingresar", redirecciona al menú para empleados
     @GetMapping("/system")
-    public ModelAndView system(@RequestParam String username, HttpSession session, Principal principal) {
-        System.out.println(principal.getName());
-        userService.loadUserByUsername(username);
-        return new ModelAndView ("index-emp");
+    public ModelAndView system() {
+        return new ModelAndView ("index-emp-new");
     }
     // al ingresar user y pw, este get mapping redirije al index del empleado
    // @GetMapping("/emp")
@@ -82,11 +91,20 @@ public class LoginController {
 
     //Al ingresar el token, redirecciona al menú para clientes
     @PostMapping("/ingreso")
-    public RedirectView menu(@RequestParam Long token, HttpSession session) {
+    public RedirectView menu(@RequestParam Long token, HttpSession session, HttpServletRequest request) {
         Table assigned_table = (Table) session.getAttribute("assigned_table");
         //service que comprueba codigo de acceso
         Boolean access = tableService.checkAccessCode(token, assigned_table.getAccess_code());
         if (access) {
+            UsernamePasswordAuthenticationToken authReq = new UsernamePasswordAuthenticationToken("cliente", "root");
+            authReq.setDetails(new WebAuthenticationDetails(request));
+            Authentication auth = authManager.authenticate(authReq);
+
+            SecurityContext sc = SecurityContextHolder.getContext();
+            sc.setAuthentication(auth);
+
+            session.setAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY, sc);
+
             return new RedirectView("/menu");
         } else {
             return new RedirectView("/guest");
