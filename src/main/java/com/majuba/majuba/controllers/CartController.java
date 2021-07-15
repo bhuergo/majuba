20a8f0b026ca7a4c9ea3e6e83d917d83749442e2
bhuergo <barbarahuergo@gmail.com>
2021-07-15 10:20:16 -0300

    CartService y CartController

--- src/main/java/com/majuba/majuba/controllers/CartController.java
+++ src/main/java/com/majuba/majuba/controllers/CartController.java
@@ -1,9 +1,14 @@
 package com.majuba.majuba.controllers;
 
+import com.majuba.majuba.services.CartService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class CartController {
+
+    @Autowired
+    private CartService cartService;
 
 
 }
---
+++ src/main/java/com/majuba/majuba/services/CartService.java
@@ -0,0 +1,9 @@
+package com.majuba.majuba.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CartService {
+
+
+}
