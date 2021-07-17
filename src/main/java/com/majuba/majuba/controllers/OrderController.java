c6efc97863e0d449a7db439f2a32975f00177641
bhuergo <barbarahuergo@gmail.com>
2021-07-17 18:25:10 -0300

    Logout config settings

--- src/main/java/com/majuba/majuba/controllers/OrderController.java
+++ src/main/java/com/majuba/majuba/controllers/OrderController.java
@@ -1,7 +1,6 @@
 package com.majuba.majuba.controllers;
 
 
-import com.majuba.majuba.entities.FoodDTO;
 import com.majuba.majuba.entities.Order;
 import com.majuba.majuba.services.CartService;
 import com.majuba.majuba.services.OrderService;
@@ -10,12 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
-
-import java.util.List;
 
 @Controller
 public class OrderController {
